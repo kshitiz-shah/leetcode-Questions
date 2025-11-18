94d0989074fb8f85322f820c20e3bfb09890f66b
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2025-11-18 10:50:53 +0530

    Time: 0 ms (100%), Space: 13 MB (72.06%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
        int i = 0;
-       int n = bits.size();
-
-       while( i < bits.size()){
-        if(i == n-1 )return true;
-        if( i == n) return false;
-        if( bits[i] == 0){
-            i++;
+        int n = bits.size();
+        
+        while (i < n - 1) {   // stop before last bit
+            if (bits[i] == 1)
+                i += 2;      // skip 2 bits
+            else
+                i += 1;      // skip 1 bit
         }
-        else{
-            i+= 2;
-        }
-
-       }
-       return false;
+        
+        // If we are exactly at last index → one-bit character
+        return i == n - 1;
     }
 };
