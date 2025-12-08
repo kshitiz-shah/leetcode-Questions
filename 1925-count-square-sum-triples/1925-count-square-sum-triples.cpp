5ed917a188aa8780f6768aa79e327726267c377b
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2025-12-08 13:49:31 +0530

    Time: 6 ms (68.58%), Space: 7.9 MB (55.75%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -7,11 +7,13 @@ public:
 
         for(int i = 1 ;i<= n; i++){
             for(int  j= 1 ; j <= n ; j++){
-                for(int  k= 1 ;k <= n ;k++){
-                    if( ((i*i) + (j * j))  == (k* k)){
-                        count++;
-                    }
-                }
+                
+                    int sum = (i*i) + (j * j);
+                    int k = (int)sqrt(sum);
+
+                    if( ((k*k) ==  sum )&& k <= n) count++;
+                    
+                
             }
         }
         return count ;
