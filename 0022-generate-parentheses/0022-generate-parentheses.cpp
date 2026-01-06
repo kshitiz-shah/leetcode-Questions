f14d1a04f34df2671cd409c7ac4a2fbd3381afe4
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-01-06 10:32:56 +0530

    Time: 5 ms (17.42%), Space: 16.4 MB (13.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
+
+        vector <string> ans ;
+        // int ind = 0;
+        int left = 0 ;
+        int right = 0;
+        string s = "" ;
+
+        solver( left, right , n ,s, ans);
+
+
+        return ans ;
         
-
-   vector<string> ans ;
-
-helper( 0, 0 , n , "" , ans);
-
-return ans;  
     }
 
-void helper (int open ,int close , int n , string s ,vector <string> &ans){
+    void solver(int left ,int right ,int n, string s , vector <string> &ans){
 
-    if( close > open || open > n )return ;
+        if(left < right) return ;
+        if(left + right > 2 * n )return ;
+        if(left > n || right > n)return ;
 
-    if( (close + open ) == (2* n)  ){
-        ans.push_back(s);
-        return ;
+        if((left + right == 2 * n) && (left == right)){
+           ans.push_back(s);
+           return ;
+        } 
+
+        solver(left + 1 , right , n, s + '(' , ans);
+        solver(left , right + 1, n, s + ')' , ans);
+
     }
-
-    if( open < n){
-        helper(open+1 ,close , n , s + '(' ,ans);
-    }
-
-    if( close < open ){
-        helper(open ,close+1 , n , s + ')' ,ans);
-    }
-
-}
 };
