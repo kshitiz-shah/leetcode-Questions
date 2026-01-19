56782b86beafa2ffe2bda2452f1743e433196656
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-01-19 12:29:27 +0530

    Time: 19 ms (72.68%), Space: 85.5 MB (47.24%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,51 +1,64 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& nums) {
+    int largestRectangleArea(vector<int>& heights) {
 
-        int n = nums.size();
+        int n = heights.size();
+
+        vector <int> pse(n , -1);
+        vector <int> nse(n , n);
         stack <int> st;
-       
 
-         vector <int> pse(n ,-1) ;
-        vector <int> nse(n, n) ;
+// pse
+       for(int i = 0 ; i < n ; i++){
 
-        int ans  = 0 ;
-
-        for(int i = 0 ;i< n ;i++){
-            while(!st.empty() && nums[st.top()] >= nums[i]){
-               
-              st.pop();
-
-            }
-            if(!st.empty() && nums[st.top()] < nums[i]){
-               
-               pse[i]=  st.top();
-               
-
-            }
-            st.push(i);      
+        while(!st.empty() && heights[st.top()] >= heights[i]){
+            st.pop();
+            
+        }
+        if(!st.empty()){
+            pse[i] = st.top();
         }
 
-         stack <int> st2;
 
-         for(int i = n-1 ; i >= 0 ; i--){
+        st.push(i);
 
-            while( !st2.empty() && nums[st2.top()] >= nums[i]){
-                st2.pop();
-            }
-            if(!st2.empty() && nums[st2.top()] <  nums[i]){
-                nse[i]= st2.top();
-            }
-            st2.push(i);
+       }
 
-         }
+       while(!st.empty()){
+        st.pop();
 
-         for(int i = 0 ;i < nums.size();i++){
+       }
 
-            ans = max(ans , (nums[i] * (nse[i] - pse[i] -1 )) );
-         }
-         return ans;
+       //nse 
 
+       for(int i = n-1 ; i>= 0 ;i--){
+
+        while(!st.empty() && heights[st.top()] >= heights[i]){
+            st.pop();
+        }
+
+         if(!st.empty()){
+            nse[i] = st.top();
+        }
+
+
+        st.push(i);
+
+
+       }
+       int ans = 0 ;
+       for(int i = 0 ; i< n ;i++){
+        int temp = heights[i] * ( nse[i] - pse[i] - 1);
+        ans =  max( ans , temp);
+
+        
+
+       }
+
+
+
+
+  return ans ;
         
     }
 };
