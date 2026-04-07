7d8aac20cc4a8cd8d3f34ca9c238c2492cb2a601
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-04-07 11:01:55 +0530

    Time: 0 ms (100%), Space: 9.8 MB (31.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,45 +1,50 @@
 class MyQueue {
 public:
-   stack <int> s1,s2;
-    MyQueue() {    
+
+   stack <int> s1  ,s2;
+    MyQueue() {
         
     }
     
     void push(int x) {
-        s1.push(x);
-
         
+        s1.push(x);
     }
     
     int pop() {
         if(s2.empty()){
-         while(!s1.empty()){
-            int k  = s1.top();
-            s2.push(k);
-            s1.pop();
-        }
+            while(!s1.empty()){
+                int k = s1.top();
+                s2.push(k);
+                s1.pop();
+            }
+           
         }
         int val = s2.top();
-        s2.pop();
-        return val ;
+           s2.pop();
+          return val ;
         
     }
     
     int peek() {
         if(s2.empty()){
-         while(!s1.empty()){
-            int k  = s1.top();
-            s2.push(k);
-            s1.pop();
+            while(!s1.empty()){
+                int k = s1.top();
+                s2.push(k);
+                s1.pop();
+            }
+         
+
         }
-        }
-        int val = s2.top();
+         return s2.top();
+
         
-        return val ;
     }
     
     bool empty() {
-        return s1.empty() && s2.empty();
+
+      
+    return s1.empty() && s2.empty();
 
         
     }
