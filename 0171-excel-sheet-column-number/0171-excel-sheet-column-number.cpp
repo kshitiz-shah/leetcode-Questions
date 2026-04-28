fd00c8955a97fef1c01d8aa752d38c7a7f2aeace
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-04-29 00:21:58 +0530

    Time: 0 ms (100%), Space: 8.7 MB (75.01%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        long long ans = 0 ;
+
+        for(char x : columnTitle){
+            ans =  ans * 26 +( x - 'A' +1 ) ;
+
+        }
+        return ans;
+        
+    }
+};
