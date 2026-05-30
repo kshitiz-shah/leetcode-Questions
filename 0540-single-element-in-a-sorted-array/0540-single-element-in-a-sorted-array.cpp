fc9025783c499950546f22c9a0b2d71400147b4e
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-05-30 07:36:34 +0530

    Time: 0 ms (100%), Space: 26.2 MB (67.57%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-             int n = nums.size(); // Size of the array.
-
-        // Edge cases:
-        if (n == 1) return nums[0];
+        int n = nums.size();
+        int left = 1 ; 
+        int right = nums.size()-2 ;
+         if (n == 1) return nums[0];
         if (nums[0] != nums[1]) return nums[0];
         if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
 
-        int low = 1, high = n - 2;
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while(left <= right){
+            int mid = left + (right -left)/2;
+            if(mid %2 == 1){
+                if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])return nums[mid ];
+                else if(nums[mid] != nums[mid-1])right = mid -1 ;
+                else left = mid +1 ;
 
-            // If nums[mid] is the single element:
-            if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
-                return nums[mid];
-            }
+            }else{
+                if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])return nums[mid ];
+                else if(nums[mid] != nums[mid+1])right = mid -1 ;
+                else left = mid +1 ;
 
-            // We are in the left part:
-            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1])
-                || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
-                // Eliminate the left half:
-                low = mid + 1;
-            }
-            // We are in the right part:
-            else {
-                // Eliminate the right half:
-                high = mid - 1;
             }
         }
-
-        // Dummy return statement:
-        return -1;
+        return -1 ;
+        
     }
 };
