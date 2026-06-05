ea483539dbe41fa3a3b162f451e54d2a3e433148
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-06-05 12:05:44 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.81%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,24 +12,26 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-
         vector <int> ans ;
-
-        func(root ,0, ans);
+        solve(root ,0,ans);
 
         return ans;
+
+
         
     }
-    void func(TreeNode* root , int level ,vector <int> &ans){
+    void solve(TreeNode* root ,int level , vector <int> &ans){
 
-        if(root == nullptr){
-            return ;
-        }
+        if(root == nullptr)return ;
+
         if(ans.size() == level){
             ans.push_back(root->val);
         }
-        func(root->right , level + 1 , ans);
-         func(root->left , level + 1 , ans);
+
+        solve(root->right ,level + 1 , ans);
+         solve(root->left ,level + 1 , ans);
+
+
 
     }
 };
