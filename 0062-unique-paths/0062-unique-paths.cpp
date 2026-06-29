d2b9fe4e2b6bb06d044c029cad5ce22a27fda324
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-06-29 18:32:18 +0530

    Time: 0 ms (100%), Space: 9.5 MB (5.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        vector<vector <int >> dp(m+1 , vector <int>(n+1 ,-1));
-      int ans =  dppath(m,n,dp);
 
-        return ans ;
+        vector <vector <int>> dp(m , vector <int> (n , -1));
         
+       return solve(0 , 0 , m , n ,dp);
+        
+
     }
+    int solve(int i , int j , int m ,int n , vector <vector <int>> &dp){
 
-   int dppath(int m , int n , vector<vector <int  >> &dp ){
+        if(i == m-1 && j == n-1)return 1 ;
+        if( i == m || j == n )return 0;
+        if(dp[i][j] != -1)return dp[i][j] ;
 
-        if(m == 1 && n ==1){
-           return 1 ;
-        }
-        if(m == 0 || n == 0)return  0;
-        if(dp[m][n] != -1)return dp[m][n];
+        int right = solve( i+1 , j ,m , n , dp);
+        int down = solve( i , j +1 , m , n, dp);
 
-        return  dp[m][n] =  dppath(m-1 , n , dp) + dppath(m , n-1 , dp);
-
+        return dp[i][j] =  right + down ;
+        
     }
 };
