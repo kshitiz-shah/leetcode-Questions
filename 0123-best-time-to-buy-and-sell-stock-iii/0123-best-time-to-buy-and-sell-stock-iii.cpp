3747dd757aa9d302ca495a5869609e4b303204c1
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-03 20:18:17 +0530

    Time: 442 ms (29.87%), Space: 224.1 MB (16.44%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n =prices.size();
+       vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
+      return  solve( prices , n , 1 , 2 , 0 ,dp);
+
+    }
+
+ int solve(vector <int> &arr , int n , int buy , int chances ,  int ind , vector <vector <vector<int>>> &dp ){
+           
+           if(ind == n || chances == 0)return 0;
+           int profit = 0 ;
+
+           if(dp[ind][buy][chances] != -1)return dp[ind][buy][chances];
+
+           if(buy){
+            profit = max((-arr[ind] + solve(arr , n , 0 , chances , ind +1 , dp)) , solve(arr ,n , 1 , chances  , ind +1  ,dp));
+
+           }else{
+            profit = max((arr[ind] + solve(arr , n , 1 , chances-1, ind +1 ,dp)) , solve(arr ,n , 0 , chances   , ind +1 ,dp) );
+
+
+           }
+           return dp[ind][buy][chances] = profit ;
+        
+    }
+};
