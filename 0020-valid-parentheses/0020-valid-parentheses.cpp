6b8fdbfd192991fe136e562f983eaca74d1cb346
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-12 10:59:44 +0530

    Time: 3 ms (9.12%), Space: 9 MB (18.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
+
         stack <char> st ;
 
-       for(char x : s){
+    for(char x : s){
+        if(st.empty() && (x == '}' || x == ']' || x == ')'))return false ;
+
+        else if (x == '(' || x == '{' || x == '[')st.push(x);
+        else if (x == ')' && st.top() != '(' || x == '}' && st.top() != '{' ||
+        x == ']' && st.top() != '['  ) return false;
+
+        else if (x == ')' && st.top() == '(' || x == '}' && st.top() == '{' ||
+        x == ']' && st.top() == '['  ) st.pop();
+    }
+    if(!st.empty())return false ;
+
+    return true ;
         
-            if( x == '(' || x == '{' || x =='['){
-                st.push(x);
-            }
-        else{
-          
-         if(st.empty())return false;
-        if( (x == '}' && st.top() == '{') || (x == ']' && st.top() == '[') ||  (x == ')' && st.top() == '(')   ){
-            st.pop();
-        }else{
-            return false ;
-        }
-        }
-       }
-       if(!st.empty())return false ;
-
-       return true ;
-
-
     }
 };
