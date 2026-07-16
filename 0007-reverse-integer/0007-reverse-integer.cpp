bc3c2eee00923285b22a76ab5824d41f3d660ca8
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-16 09:52:28 +0530

    Time: 6 ms (6.25%), Space: 8.5 MB (83.97%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,22 +5,24 @@ public:
         bool flag = false ;
 
         if(x < 0)flag = true ;
+        long long k = x;
+        if(k < 0) k = - k;
        
 
     
-      int ans = 0;
+      long long ans = 0;
 
-        while(x != 0){
-            int temp = x % 10 ;
+        while(k > 0){
+            int temp = k % 10 ;
           
             if(ans > INT_MAX/ 10 || ans < INT_MIN /10)return 0 ;
               ans = (ans* 10) + temp ;
 
-            x /= 10 ;
+            k /= 10 ;
         }
        
-
-        return  ans ;
+       if(flag == true)return (int) -ans ;
+        return (int) ans ;
         
     }
 };
