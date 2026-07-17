2c99f0e268bffd165c64547cb885a52325f6a703
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-18 03:49:36 +0530

    Time: 3 ms (26.06%), Space: 12.1 MB (8.57%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        int n = nums.size();
+
+        vector<vector<int>> ans ;
+        vector <int> temp ;
+        vector <bool>visited(n ,false);
+
+        solve(  ans ,  temp , nums , visited);
+
+        return ans ;     
+    }
+    void solve( vector<vector<int>> &ans , vector <int> temp , vector<int>& nums , vector <bool> &visited){
+
+        if(temp.size() == nums.size()){
+            ans.push_back(temp);
+            return ;
+        }
+
+        for(int i = 0 ; i< nums.size();i++){
+            if(visited[i])continue;
+           visited[i]=true ;
+            temp.push_back(nums[i]);
+
+           solve(  ans ,  temp , nums , visited);
+
+          temp.pop_back();
+            visited[i]= false ;
+
+        }
+
+
+
+
+        
+
+    }
+
+};
