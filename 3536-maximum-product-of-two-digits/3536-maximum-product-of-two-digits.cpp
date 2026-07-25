4224824a958cdccdd544a370a13e77a2d71423ea
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-25 22:48:49 +0530

    Time: 0 ms (100%), Space: 8.5 MB (89.66%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -2,32 +2,22 @@ class Solution {
 public:
     int maxProduct(int n) {
 
-        vector <int> arr ;
-        int a = 0;
-        int b = 0;
-
-      while(n > 0){
-        int rem = n %10 ;
-        arr.push_back(rem);
-        n/= 10 ;
-      }
-      int ind = -1 ;
-      for(int i = 0 ; i< arr.size();i++){
-         if(arr[i] > a){
-            a = arr[i];
-            ind = i ;
-         }
-      }
-
-      for(int i =0 ; i< arr.size();i++){
-        if(i != ind){
-            if(arr[i] > b){
-                b = arr[i];
+        int a = 0; 
+        int b = 0 ;
+        while(n > 0){
+            int rem = n %10 ;
+            if(rem > a){
+                 b =a ;
+                a = rem ;
+            
+               
+            }else if( rem > b){
+                b= rem ;
+                
             }
+            n /=10 ;
         }
-      }
-
-      return a * b ;
+        return a * b ;
         
     }
 };
