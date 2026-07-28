c3305c435d9cbafc439d0b79eaaa17d5cd158cd7
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-07-28 11:31:31 +0530

    Time: 27 ms (74.51%), Space: 54.3 MB (88.13%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,32 +2,32 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
 
-       map <char , int> mpp ;
-       
-       for(char x : s){
-        mpp[x]++ ;
-       }
-       int i = 0;
-       int j = s.size()-1;
-     
-        for( auto &a: mpp){
-        while(a.second > 0 ){
-            if(a.second != 1){
-                s[i++] = a.first ;
-            s[j--] = a.first ;
-            a.second -= 2 ;
-
-            }else{
-               s[s.size() /2] = a.first ;
-               a.second -=1 ; 
-            }
-            
-
-        }
+      vector <int> freq(26 ,0);
+      for(char x :s){
+        freq[x-'a'] +=1 ;
       }
-       
-       return s ;
+      int l = 0 ;
+      int r = s.size()-1 ;
 
+      for(int i = 0 ;i < 26 ;i++){
+        
+        while(freq[i] > 0){
+            char ch = i + 'a';
+            if(freq[i] != 1){
+                
+                s[l++] = ch ;
+                s[r--] =ch ;
+                freq[i] -= 2 ;
+            }else{
+                 s[s.size()/2] = ch ;
+                 freq[i] -= 1 ;
+            }
+        }
+      
+      
+      
+      }
+      return s ;
         
     }
 };
