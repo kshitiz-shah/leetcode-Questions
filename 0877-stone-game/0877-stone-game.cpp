80875c32163fb23822878cb1e755bcffc08c0a0a
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-08-02 20:28:07 +0530

    Time: 16 ms (16.99%), Space: 19.8 MB (11.06%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+
+      vector<vector<int>> dp(n ,vector<int>(n,-1)) ;
+
+       return solve(0 , n-1 , piles,dp) > 0 ? true:false;
+
+
+        
+    }
+    int solve(int left , int right , vector <int> &piles ,vector<vector<int>> &dp){
+        if(left > right)return 0 ;
+        if(dp[left][right] != -1)return dp[left][right];
+
+        int leftmax = piles[left] - solve(left +1 , right, piles,dp) ;
+        int rightmax = piles[right] -solve(left , right -1, piles,dp) ;
+
+        return dp[left][right] = max(left ,right);
+
+        
+    }
+};
