024da84256ea8b15695f3f09493a3861e89e37a3
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-08-04 00:00:58 +0530

    Time: 19 ms (32.65%), Space: 16.8 MB (26.97%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -4,25 +4,23 @@ public:
 
          int n = piles.size();
 
-        vector<int> suffix(n);
-        suffix[n - 1] = piles[n - 1];
+       
+     
 
-        for (int i = n - 2; i >= 0; i--)
-            suffix[i] = suffix[i + 1] + piles[i];
-
+        
         vector<vector<vector<int>>> dp(
             2,
             vector<vector<int>>(n, vector<int>(n + 1, INT_MIN))
         );
 
-        return solve(true, 0, 1, piles, suffix, dp);
+        return solve(true, 0, 1, piles, dp);
     }
 
     int solve(bool aliceTurn,
               int index,
               int M,
               vector<int>& piles,
-              vector<int>& suffix,
+             
               vector<vector<vector<int>>>& dp) {
 
         if (index >= piles.size())
@@ -48,7 +46,7 @@ public:
                                    index + X,
                                    max(M, X),
                                    piles,
-                                   suffix,
+                                   
                                    dp)
                 );
             }
@@ -65,7 +63,7 @@ public:
                           index + X,
                           max(M, X),
                           piles,
-                          suffix,
+                         
                           dp)
                 );
             }
