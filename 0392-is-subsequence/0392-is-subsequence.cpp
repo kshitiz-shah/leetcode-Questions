df254fc1a9f6508c7a083c0eda6a73137fe052ba
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-08-08 12:01:27 +0530

    Time: 111 ms (1.85%), Space: 283 MB (5.22%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
 
-        int ptr1 = 0;
-        int ptr2 = 0 ;
+      return  solve(s,t, 0,0);
+    }
+    bool solve(string s , string t ,int ptr1 ,int ptr2){
 
-        while( ptr1 < s.size() && ptr2 < t.size()){
-            if(s[ptr1] == t[ptr2]){
-                ptr1++;
-                ptr2++;
-            }
-            else ptr2++ ;
-        }
         if(ptr1 == s.size())return true ;
-        return false ;
-        
+        if(ptr2 == t.size()) return false ;
+
+        if(s[ptr1] == t[ptr2]) return  solve(s,t, ptr1 +1 ,ptr2 +1);
+        else return  solve(s,t, ptr1  ,ptr2 +1);
+
+
+
     }
 };
