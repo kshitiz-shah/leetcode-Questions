62335483ac876d4469e68203728d845365bd7dd7
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-08-10 20:21:00 +0530

    Time: 3 ms (30.95%), Space: 10.1 MB (39.64%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -4,21 +4,23 @@ public:
 
         int n = nums.size();
         bool aliceturn = true ;
+        vector<vector<int>> dp(n ,vector <int>(n ,INT_MIN));
 
-      if(  solve(0 , n-1 , nums ,aliceturn) >= 0 ) return true;
+      if(  solve(0 , n-1 , nums ,aliceturn ,dp) >= 0 ) return true;
       return false;
 
 
         
     }
-    int solve(int left , int right , vector <int> &nums, bool aliceturn){
+    int solve(int left , int right , vector <int> &nums, bool aliceturn , vector<vector<int>> &dp){
 
         if(left > right)return 0; 
+        if(dp[left][right] != INT_MIN)return dp[left][right];
 
         if(aliceturn){
-            return max(nums[left] + solve(left +1 , right , nums, false) , nums[right] + solve(left , right -1 ,nums, false));
+            return dp[left][right] = max(nums[left] + solve(left +1 , right , nums, false ,dp) , nums[right] + solve(left , right -1 ,nums, false,dp));
         }else{
-            return min( - nums[left] + solve(left+1 , right , nums, true) , -nums[right]+ solve(left ,right -1 ,nums ,true));
+            return dp[left][right] = min( - nums[left] + solve(left+1 , right , nums, true,dp) , -nums[right]+ solve(left ,right -1 ,nums ,true,dp));
         }
 
 
