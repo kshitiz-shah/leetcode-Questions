7e4a907710e8638a3abc57264ae6ccb9a25bdda1
Kshitij shah <153758438+kshitiz-shah@users.noreply.github.com>
2026-08-10 13:43:45 +0530

    Time: 166 ms (5.05%), Space: 9.6 MB (68.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    bool predictTheWinner(vector<int>& nums) {
+    bool predictTheWinner(vector<int>& piles) {
+          int right = piles.size()-1;
 
-        int left = 0 ;
-        int right = nums.size()-1;
-        bool aliceturn = true ;
-
-      int ans =  solve(left , right , nums,aliceturn);
-        if(ans >= 0)return true ;
-        return false ;
-
+          bool aliceturn=  true ;
+    int k = solve(0 , right , piles , true);
+    if(k >= 0 )return true ;
+    return false ;
         
     }
-    long long solve(int left ,int right, vector <int> & nums, bool aliceturn){
 
-        if(left > right)return 0;
-       long long ans = 0 ;
+    int solve(int left , int right , vector <int> &piles , bool aliceturn){
+
+        if(left > right)return 0 ;
+        int ans = 0 ;
         if(aliceturn){
-            ans +=  max(nums[left] + solve(left+ 1 , right , nums,false),
-             nums[right] + solve(left ,right-1 , nums ,false));
+            ans += max(piles[left] + solve(left +1 , right , piles , false) , piles[right] + solve(left , right -1 , piles, false));
+
+        }else{
+            ans += min(-piles[left] + solve(left +1 , right , piles , true) , -piles[right] + solve(left , right -1 , piles, true) );
+
 
         }
-        else{
-            ans -= max(nums[left] + solve(left+ 1 , right , nums,false),
-             nums[right] + solve(left ,right-1 , nums ,false));
-        }
-
         return ans ;
-
+        
     }
 };
